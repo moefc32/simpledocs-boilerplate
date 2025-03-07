81c8fb12ab52689d3dba3cdb7f87977835cd7bdb
Faizal Chan. <moefc32@gmail.com>
2025-03-07 10:54:22 +0700

    Updates

---
+++ _app/immutable/nodes/2.CSdu-ZzX.js
@@ -0,0 +1,8 @@
+import{t as r,h as p,a as i,s as l}from"../chunks/DtJBtnD3.js";import{i as c}from"../chunks/DU5zlBa9.js";import{p as d,f,t as s,a as h,$ as q,c as v,r as x,n as b}from"../chunks/CCxjcWvL.js";import{s as g}from"../chunks/B6GSLE5E.js";var _=r('<meta property="og:title">'),S=r(`<h1 class="mt-3 mb-6 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis neque
+    voluptatum repudiandae hic dolore tenetur rem atque aliquam sint deleniti
+    quod, veniam amet, nemo iusto excepturi velit eum? Aut, magni praesentium
+    totam eos maiores accusamus nihil a pariatur! Sit rerum, distinctio nulla
+    ipsa est possimus! Possimus nesciunt, repudiandae ullam enim nam facere.
+    Laudantium, recusandae! Nemo consequatur reprehenderit in quia illum,
+    facilis rem libero exercitationem similique voluptatibus ratione saepe
+    sapiente reiciendis molestias fuga, sit quasi ad aliquid quos omnis.</p>`,1);function $(o,m){d(m,!1),c();var e=S();p(n=>{var a=_();s(()=>{q.title="Simpledocs Boilerplate",g(a,"content","Simpledocs Boilerplate")}),i(n,a)});var t=f(e),u=v(t,!0);x(t),b(2),s(()=>l(u,"Simpledocs Boilerplate")),i(o,e),h()}export{$ as component};
