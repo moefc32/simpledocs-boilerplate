18a0ecdbf2cab8c4178a8b5f444c787d6047f90b
Faizal Chan. <moefc32@gmail.com>
2025-06-20 18:47:20 +0700

    Updates

--- _app/immutable/nodes/2.BFGljLN6.js
+++
@@ -1,8 +0,0 @@
-import{t as n,h as m,a as s,s as h}from"../chunks/CkPMGfKk.js";import{i as g}from"../chunks/ClzKPE17.js";import{p as f,t as i,a as u,$ as x,c as o,r,n as y}from"../chunks/BYOHr42B.js";import{s as b}from"../chunks/Czmk2fT1.js";var v=n('<meta property="og:title">'),w=n(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p><span class="inline-flex gap-2"><a href="https://opensource.org/license/mit" target="_blank"><img src="https://img.shields.io/badge/License-MIT-green?style=flat-square" class="h-[24px]"></a> <a href="https://pnpm.io/" target="_blank"><img src="https://img.shields.io/badge/Package-PNPM-orange?style=flat-square" class="h-[24px]"></a> <img src="https://img.shields.io/badge/Module-ECMAScript-yellow?style=flat-square" class="h-[24px]"></span></p> <p>Simpledocs is a minimalistic and efficient documentation site designed
-        for designers, developers, and teams who need a quick and organized way
-        to present their documentation. It offers a clean and responsive layout
-        with essential features like a navigation sidebar and search
-        functionality. Built with simplicity in mind, this project ensures that
-        users can focus on writing content without dealing with complex
-        configurations. Its lightweight nature makes it easy to deploy on the
-        GitHub Pages.</p></div>`);function B(l,p){f(p,!1),g();var e=w();m(d=>{var a=v();i(()=>{x.title="Simpledocs Boilerplate",b(a,"content","Simpledocs Boilerplate")}),s(d,a)});var t=o(e),c=o(t,!0);r(t),y(4),r(e),i(()=>h(c,"Simpledocs Boilerplate")),s(l,e),u()}export{B as component};
